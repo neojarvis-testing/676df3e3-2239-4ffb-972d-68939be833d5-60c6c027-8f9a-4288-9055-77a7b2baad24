0e9b2d70f552cedb91d6016dfef1fffaf0b687d6
Divyanshgour7-gour <divyanshgour007@gmail.com>
2025-04-10 14:11:07 +0000

    Initial Commit

---
+++ Project/src/test/java/uistore/ProductBuyingLocatorsDiv.java
@@ -0,0 +1,15 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class ProductBuyingLocatorsDiv {
+
+
+    public static By braceletbuy=By.cssSelector("a[href='/Messika-18k-Yellow-Gold-My-Twin-Toi-and.-Moi-0.35cttw-Diamond-Wire-Memory-bracelet-+-Size-Medium-07222+YG+M/p/37710711']");
+    public static By addtocart=By.cssSelector("#addToCartSubmit");
+    public static By ringbuy=By.cssSelector("#productTile-08550072");
+    public static By livehelp=By.cssSelector(".atcLiveButton");
+
+
+    
+}
